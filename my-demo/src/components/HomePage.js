87b6e9acf90ff0b1ea906bc406b24685071811af
Thinh Nguyen <thanhthinhcntt@gmail.com>
2019-12-07 15:39:46 +0700

    move mock data and add homepage components

---
+++ my-demo/src/components/HomePage.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+
+/**
+* @author Thinh Nguyen
+* @class HomePage
+**/
+
+class HomePage extends Component {
+    state = {}
+    render() {
+        return (
+            <div>HomePage</div>
+        )
+    }
+}
+
+
+
+export default HomePage
